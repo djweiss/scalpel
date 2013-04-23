1535fa33af50a82d152cce2e04ccbaac39541efc
David Weiss <djw500@gmail.com>
2013-04-23 17:41:39 -0400

    added licenses

--- scalpel/+scalpel/config.m
+++ scalpel/+scalpel/config.m
@@ -1,6 +1,29 @@
 function opts = config(varargin)
 % Edit this to point to where input/outputs should go
 
+% ======================================================================
+% Copyright (c) 2012 David Weiss
+% 
+% Permission is hereby granted, free of charge, to any person obtaining
+% a copy of this software and associated documentation files (the
+% "Software"), to deal in the Software without restriction, including
+% without limitation the rights to use, copy, modify, merge, publish,
+% distribute, sublicense, and/or sell copies of the Software, and to
+% permit persons to whom the Software is furnished to do so, subject to
+% the following conditions:
+% 
+% The above copyright notice and this permission notice shall be
+% included in all copies or substantial portions of the Software.
+% 
+% THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+% EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+% MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+% NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE
+% LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION
+% OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION
+% WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+% ======================================================================
+
 defaults.model_file = './data/scalpel_voc2012train_final.mat';
 defaults.working_dir = './work'; 
 defaults.src_dir = fileparts(which('startup'));
--- scalpel/register_rhulls.m
+++ scalpel/register_rhulls.m
@@ -1,6 +1,29 @@
 function A=register_rhulls(rhull1,rhull2);
 % Timothee Cour, 06-Jun-2008 05:41:57 -- DO NOT DISTRIBUTE
 
+% ======================================================================
+% Copyright (c) 2012 David Weiss
+% 
+% Permission is hereby granted, free of charge, to any person obtaining
+% a copy of this software and associated documentation files (the
+% "Software"), to deal in the Software without restriction, including
+% without limitation the rights to use, copy, modify, merge, publish,
+% distribute, sublicense, and/or sell copies of the Software, and to
+% permit persons to whom the Software is furnished to do so, subject to
+% the following conditions:
+% 
+% The above copyright notice and this permission notice shall be
+% included in all copies or substantial portions of the Software.
+% 
+% THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+% EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+% MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+% NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE
+% LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION
+% OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION
+% WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+% ======================================================================
+
 xy1=rhull2rectangle(rhull1);
 xy2=rhull2rectangle(rhull2);
 A=registerPoints(xy1,xy2);
