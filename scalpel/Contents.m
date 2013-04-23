b5619e897291a4fe8400e928f262b510127ac045
David Weiss <djw500@gmail.com>
2013-04-23 18:00:11 -0400

    should work now

--- scalpel/Contents.m
+++ scalpel/Contents.m
@@ -1,1 +1,13 @@
-% 
+% Segmentation CAscades with Localized Priors and Efficient Learning
+% Version 1 04-23-2013
+%
+% By David Weiss & Ben Taskar. See scalpel_demo.m for information on how 
+% to use the toolbox.
+%
+% SCALPEL Setup
+%   SCALPEL.config     - Set up where output, working files should go
+%
+% SCALPEL Demo
+%   scalpel_demo.m     - Demo instructions
+%   startup.m          - Paths setup (should work automatically)
+
